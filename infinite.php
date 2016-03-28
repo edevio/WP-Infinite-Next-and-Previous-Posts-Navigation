0a533f914804079260743012ea9afbd6add47473
Edward Robinson <edward@mbp.com>
2016-03-28 22:59:32 +0100

    Adds support for pre php 5.3 ternary support | Adds a few more comments

--- infinite.php
+++ infinite.php
@@ -37,48 +37,55 @@
  * Returns first post if no more Next posts and Last if no more Previous posts
  *
  */
-function mbo_get_infinite_adjacent_navigation( $insert_element = '', $previous_button_text = 'Previous', $next_button_text = 'Next' ){
+function edev_get_infinite_adjacent_navigation( $insert_element = '', $previous_button_text = 'Previous', $next_button_text = 'Next' ){
 
 	global $wp_query;
 
 	$post_type = ( isset( $wp_query->queried_object->post_type ) ? $wp_query->queried_object->post_type : 'post' );
 
-		// Get posts not in the same Tax Term, not excluding any and either previous or !previous (next)
-		$prev_post  = get_adjacent_post( false, '', true );
-    $next_post  = get_adjacent_post( false, '', false );
+	// Get posts not in the same Tax Term, not excluding any and either previous or !previous (next)
+	$prev_post  = get_adjacent_post( false, '', true );
+	$next_post  = get_adjacent_post( false, '', false );
 
-    $nav_output = NULL;
+	$nav_output = NULL;
 
+	// If have previous posts construct
 	if ( ! empty( $prev_post ) ) {
-		$link_text = ( $previous_button_text ? : $prev_post->post_title );
+		$link_text = ( $previous_button_text ? $previous_button_text : $prev_post->post_title );
 		$nav_output .= "<a class='post-link previous-link' href='{$prev_post->guid}'>{$link_text}</a>";
 	}
+	// If no more previous posts i.e. If at start of all posts, query for 1 and last post
 	else{
 		$args = array(
 			'post_type'   		=> $post_type,
 			'post_per_page' 	=> 1,
 			'order'			    	=> 'DESC'
 		);
+
 		$single_post = new WP_Query( $args ); $single_post->the_post();
-		$link_text   = ( $previous_button_text ? : get_the_title() );
+		$link_text   = ( $previous_button_text ? $previous_button_text : get_the_title() );
 		$nav_output .= '<a class="post-link previous-link" href="' . get_permalink() . '">' . $link_text .'</a>';
 		wp_reset_query();
+
 	}
 
+	// Inserts any element in between previous and next posts. Useful for inserting items like generic 'back to index' link
 	$nav_output .= $insert_element;
 
+	// If have next posts construct
 	if ( ! empty( $next_post ) ) {
-		$link_text = ( $next_button_text ? : $next_post->post_title );
+		$link_text = ( $next_button_text ? $next_button_text : $next_post->post_title );
 		$nav_output .= "<a class='post-link next-link icon-right-side-padding' href='{$next_post->guid}'>{$link_text}</a>";
 	}
+	// If no more next posts i.e. If at end of all posts, query for 1 and first post
 	else{
 		$args = array(
 			'post_type'   		=> $post_type,
 			'post_per_page' 	=> 1,
-			'order'				=> 'ASC'
+			'order'						=> 'ASC'
 		);
 		$single_post = new WP_Query( $args ); $single_post->the_post();
-		$link_text   = ( $next_button_text ? : get_the_title() );
+		$link_text   = ( $next_button_text ?  $next_button_text : get_the_title() );
 		$nav_output .= '<a class="post-link next-link icon-right-side-padding" href="' . get_permalink() . '">' . $link_text . '</a>';
 		wp_reset_query();
 	}
@@ -87,9 +94,9 @@ function mbo_get_infinite_adjacent_navigation( $insert_element = '', $previous_b
 
 }
 
-add_filter ('the_content', 'insertContent');
+add_filter ('the_content', 'edev_insert_after_content');
 
-function insertContent( $content ) {
-	$content .= mbo_get_infinite_adjacent_navigation();
+function edev_insert_after_content( $content ) {
+	$content .= edev_get_infinite_adjacent_navigation();
 	return $content;
 }
